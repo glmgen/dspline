60e2ac9dceccd507345e6d9c2da1aa3da7188f7f
Ryan Tibshirani <ryantibs@gmail.com>
2022-07-19 15:28:31 -0700

    Add vector/matrix check

--- R/checks.R
+++ R/checks.R
@@ -1,6 +1,9 @@
 check_length <- function(x, n, op = c("==", ">=", "<=")) {
   args = as.character(as.list(match.call())[-1])
   op = match.arg(op)
+  if (!(is.vector(x) && is.atomic(x))) {
+    rlang::abort(sprintf("`%s` must be an atomic vector.", args[1]))
+  }
   if (op == "==") {
     if (length(x) != n) {
       rlang::abort(sprintf("`length(%s)` must equal `%s`.", args[1], args[2]))
@@ -20,6 +23,9 @@ check_length <- function(x, n, op = c("==", ">=", "<=")) {
 
 check_rows <- function(x, n) {
   args = as.character(as.list(match.call())[-1])
+  if (!is.matrix(x)) {
+    rlang::abort(sprintf("`%s` must be a matrix.", args[1]))
+  }
   if (nrow(x) != n) {
     rlang::abort(sprintf("`nrow(%s)` must equal `%s`.", args[1], args[2]))
   }
@@ -27,6 +33,9 @@ check_rows <- function(x, n) {
 
 check_cols <- function(x, n) {
   args = as.character(as.list(match.call())[-1])
+  if (!is.matrix(x)) {
+    rlang::abort(sprintf("`%s` must be a matrix.", args[1]))
+  }
   if (ncol(x) != n) {
     rlang::abort(sprintf("`ncol(%s)` must equal `%s`.", args[1], args[2]))
   }
